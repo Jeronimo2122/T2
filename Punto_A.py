e5ac500aec05fbda4f5dc94b09b33dfa6e8ce06c
Juan Lopez <je.lopezu1@uniandes.edu.co>
2024-09-18 12:46:34 -0500

    Visualizacion del punto A

--- Punto_A.py
+++ Punto_A.py
@@ -1,10 +1,10 @@
 # Importar Datos de Drones
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import gurobipy as gp
 import networkx as nx
+import tabulate as tb
 
 # Importar Datos xlsx (Primera fila nombres de columnas)
 data = pd.read_excel('Tarea 2-202420.xlsx', header=1)
@@ -12,11 +12,13 @@ data = pd.read_excel('Tarea 2-202420.xlsx', header=1)
 # Crear DataFrame
 df = pd.DataFrame(data)
 
+fotos = list(df['Fotos a tomar'])
+
 # Mostrar DataFrame
 print(df.head())
 
 # Conjunto 
-Drones = [0,1,2,3,4]
+Drones = [1,2,3,4,5]
 Lugares = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,21,22,23,24,25]
 
 # Lista de arcos entre lugares
@@ -24,8 +26,6 @@ A = [(i,j) for i in Lugares for j in Lugares if i != j]
 
 # Diccionario de distancia entre lugares (Llave tupla de lugares)
 q = {(i,j): float(abs(df['Calle'][i]-df['Calle'][j]) + abs(df['Carrera'][i]-df['Carrera'][j])) for i,j in A}
-
-print(A)
 
 # Crear Modleo de Optimización
 m = gp.Model('Drones')
@@ -66,7 +66,36 @@ m.optimize()
 print('Estatus: ', m.Status)
 print('Función Objetivo: ', m.objVal)
 
-#Imprimir grafo de solución
+Gp = nx.DiGraph()
+for i,j in A:
+    if x[i,j].x > 0.1:
+        Gp.add_edge(i,j)
+cycles = list(nx.simple_cycles(Gp.to_directed()))
+
+f = []
+for i in list(range(len(cycles))):
+    p = 0
+    for j in list(range(len(cycles[i]))):
+        p = p + fotos[cycles[i][j]]
+    f.append(p)
+
+# necesito sacar las distancias de cada ciclo con las distancias de q
+distancias = []
+for i in list(range(len(cycles))):
+    l = 0
+    for j in list(range(len(cycles[i])-1)):
+        l = l + (q[cycles[i][j], cycles[i][j+1]] / 1000)
+    distancias.append(l)
+
+data = list(zip(cycles, distancias, f))
+
+headers = ['Ciclos', 'Tiempo', 'Fotos']
+
+table = tb.tabulate(data, headers= headers, tablefmt='fancy_grid')
+
+print(table)
+
+'''#Imprimir grafo de solución
 G = nx.DiGraph()
 G.add_nodes_from(Lugares)
 for i,j in A:
@@ -75,21 +104,5 @@ for i,j in A:
             G.add_edge(i,j)
 
 nx.draw(G, with_labels=True)
-plt.show()
-
-#Imprimir solución
-'''for d in Drones:
-    for i,j in A:
-        if x[i,j].x > 0.1:
-            print('Dron ', d, ' va de ', i, ' a ', j)'''
-
-
-
-
-
-
-
-
-
-
+plt.show()'''
 
