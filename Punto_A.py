1c6eb2d081d3cd2a6211f132536606d6ae058bf5
Juan Lopez <je.lopezu1@uniandes.edu.co>
2024-09-18 11:19:10 -0500

    Punto A con resp de 334

--- Punto_A.py
+++ Punto_A.py
@@ -28,52 +28,39 @@ q = {(i,j): float(abs(df['Calle'][i]-df['Calle'][j]) + abs(df['Carrera'][i]-df['
 print(A)
 
 # Crear Modleo de Optimización
-
 m = gp.Model('Drones')
 
 #Variables de decisión
 #x_dij:  {█(■(1&Si el drone d∈D debe ir del lugar i∈L hacia el lujar j∈L.),@0&En otro caso.).
-x = m.addVars(Drones, Lugares, Lugares, vtype=gp.GRB.BINARY, name='x')
+x = m.addVars(Lugares, Lugares, vtype=gp.GRB.BINARY, name='x')
 
 #Función Objetivo
 #Minimizar: ∑_((i,j)∈A)▒∑_(d∈D)▒〖X_dij d_ij 〗  
-m.setObjective(gp.quicksum(q[i,j]*x[d,i,j] for i,j in A for d in Drones), sense=gp.GRB.MINIMIZE)
+m.setObjective(gp.quicksum(q[i,j]*x[i,j] for i,j in A), sense=gp.GRB.MINIMIZE)
+
+
 
 #Restricciones
 #	Todo cultivo debe ser visitado por un dron.
 #∑_(d∈D)▒∑_({j:(j,i)∈A})▒x_dij =1 ; ∀ i∈L 
 for i in Lugares:
     if i != 0:
-        m.addConstr(gp.quicksum(x[d,j,i] for d in Drones for j in Lugares if (j,i) in A) == 1)
-        m.addConstr(gp.quicksum(x[d,j,i] for d in Drones for j in Lugares if (i,j) in A) == 1)
-
-'''
-#∑_(d∈D)▒∑_({j:(i,j)∈A})▒x_dij =1 ; ∀ i∈L 
-for i in Lugares:
-    if i != 0:
-        m.addConstr(gp.quicksum(x[d,i,j] for d in Drones for j in Lugares if (i,j) in A) == 1)
-'''
+        m.addConstr(gp.quicksum(x[i,j] for j in Lugares) == 1)
+        m.addConstr(gp.quicksum(x[j,i] for j in Lugares) == 1)
 
 #	Se deben utilizar todos los Drones disponibles.
 #∑_((i,j)∈A)▒x_dij ≥1 ; ∀ d∈D 
-for d in Drones:
-    m.addConstr(gp.quicksum(x[d,i,j] for i,j in A) >= 1)
+m.addConstr(gp.quicksum(x[0,j] for j in Lugares) == 5)
+m.addConstr(gp.quicksum(x[i,0] for i in Lugares) == 5)
 
 #	No deben existir ciclos entre un par de ubicaciones nisiquiera por drones diferentes.
 #∑_({d:(d,i,j)∈D})▒x_dij ≤1 ; ∀ (i,j)∈A
-for i,j in A:
-    m.addConstr(gp.quicksum(x[d,i,j] for d in Drones) <= 1)
+for i in Lugares:
+    for j in Lugares:
+        m.addConstr(x[i,j] + x[j,i] <= 1)
 
-# Todos los drones deben salir del lugar 0
-#∑_({j:(0,j)∈A})▒x_d0j =1 ; ∀ d∈D
-for d in Drones:
-    m.addConstr(gp.quicksum(x[d,0,j] for j in Lugares if (0,j) in A) == 1)
 
-# Todos los drones deben regresar al lugar 0
-#∑_({i:(i,0)∈A})▒x_di0 =1 ; ∀ d∈D
-for d in Drones:
-    m.addConstr(gp.quicksum(x[d,i,0] for i in Lugares if (i,0) in A) == 1)
-
+#Resolver
 #Optimizar
 m.optimize()
 print('Estatus: ', m.Status)
@@ -84,17 +71,17 @@ G = nx.DiGraph()
 G.add_nodes_from(Lugares)
 for i,j in A:
     for d in Drones:
-        if x[d,i,j].x > 0.1:
+        if x[i,j].x > 0.1:
             G.add_edge(i,j)
 
 nx.draw(G, with_labels=True)
 plt.show()
 
 #Imprimir solución
-for d in Drones:
+'''for d in Drones:
     for i,j in A:
-        if x[d,i,j].x > 0.1:
-            print('Dron ', d, ' va de ', i, ' a ', j)
+        if x[i,j].x > 0.1:
+            print('Dron ', d, ' va de ', i, ' a ', j)'''
 
 
 
