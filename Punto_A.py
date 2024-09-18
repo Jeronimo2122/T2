8272b0a47b3839fc835315ee627e4eab2f492971
Juan Lopez <je.lopezu1@uniandes.edu.co>
2024-09-18 15:22:59 -0500

    Listo el punto A con resp 328

--- Punto_A.py
+++ Punto_A.py
@@ -1,6 +1,7 @@
 # Importar Datos de Drones
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import gurobipy as gp
 import networkx as nx
@@ -12,14 +13,13 @@ data = pd.read_excel('Tarea 2-202420.xlsx', header=1)
 # Crear DataFrame
 df = pd.DataFrame(data)
 
-fotos = list(df['Fotos a tomar'])
-
 # Mostrar DataFrame
 print(df.head())
 
 # Conjunto 
 Drones = [0,1,2,3,4]
-Lugares = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,21,22,23,24,25]
+Lugares = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+Fotos = df['Fotos a tomar'].tolist()
 
 # Lista de arcos entre lugares
 A = [(i,j) for i in Lugares for j in Lugares if i != j]
@@ -28,7 +28,6 @@ A = [(i,j) for i in Lugares for j in Lugares if i != j]
 q = {(i,j): float(abs(df['Calle'][i]-df['Calle'][j]) + abs(df['Carrera'][i]-df['Carrera'][j])) for i,j in A}
 
 # Crear Modleo de Optimización
-
 m = gp.Model('Drones')
 
 #Variables de decisión
@@ -37,95 +36,77 @@ x = m.addVars(Lugares, Lugares, vtype=gp.GRB.BINARY, name='x')
 
 #Función Objetivo
 #Minimizar: ∑_((i,j)∈A)▒∑_(d∈D)▒〖X_dij d_ij 〗  
-m.setObjective(gp.quicksum(q[i,j]*x[d,i,j] for i,j in A for d in drones), sense=gp.GRB.MINIMIZE)
+m.setObjective(gp.quicksum(q[i,j]*x[i,j] for i,j in A if i != j), sense=gp.GRB.MINIMIZE)
+
+
 
 #Restricciones
 #	Todo cultivo debe ser visitado por un dron.
 #∑_(d∈D)▒∑_({j:(j,i)∈A})▒x_dij =1 ; ∀ i∈L 
 for i in Lugares:
-    m.addConstr(gp.quicksum(x[d,j,i] for d in drones for j in Lugares if (j,i) in A) == 1)
+    if i != 0:
+        m.addConstr(gp.quicksum(x[i,j] for j in Lugares if i != j) == 1)
 
-#∑_(d∈D)▒∑_({j:(i,j)∈A})▒x_dij =1 ; ∀ i∈L 
-for i in Lugares:
-    m.addConstr(gp.quicksum(x[d,i,j] for d in drones for j in Lugares if (i,j) in A) == 1)
+for j in Lugares:
+    if j != 0:
+        m.addConstr(gp.quicksum(x[i,j] for i in Lugares if i != j) == 1)
 
-#	Se deben utilizar todos los drones disponibles.
+#	Se deben utilizar todos los Drones disponibles.
 #∑_((i,j)∈A)▒x_dij ≥1 ; ∀ d∈D 
-for d in drones:
-    m.addConstr(gp.quicksum(x[d,i,j] for i,j in A) >= 1)
+m.addConstr(gp.quicksum(x[0,j] for j in Lugares if j != 0) == 5)
+m.addConstr(gp.quicksum(x[i,0] for i in Lugares if i != 0) == 5)
 
-# Al hangar regresa y salen los 5 drones (Al hangar suma 5 y del hangar sale 5)
-#∑_({j:(j,0)∈A})▒x_d0j = 5 ; ∀ d∈D
-for d in drones:
-    m.addConstr(gp.quicksum(x[d,j,0] for j in Lugares if (j,0) in A) == 1)
-
-#	No deben existir ciclos entre un par de ubicaciones.
-#∑_(((i,j)∈(S×S)∩A))▒x_dij ≥1 ; ∀    d∈D,S⊂L,|S|=2 
-
+#	No deben existir ciclos entre un par de ubicaciones nisiquiera por drones diferentes.
+#∑_({d:(d,i,j)∈D})▒x_dij ≤1 ; ∀ (i,j)∈A
+for i in Lugares:
+    for j in Lugares:
+        if i != j:
+            m.addConstr(x[i,j] + x[j,i] <= 1)
 
 
-# Callback para Lazy Constraints
-
-
-
+#Resolver
 #Optimizar
 m.optimize()
 print('Estatus: ', m.Status)
 print('Función Objetivo: ', m.objVal)
 
-Gp = nx.DiGraph()
-for i,j in A:
-    if x[i,j].x > 0.1:
-        Gp.add_edge(i,j)
-cycles = list(nx.simple_cycles(Gp.to_directed()))
-
-f = []
-for i in list(range(len(cycles))):
-    p = 0
-    for j in list(range(len(cycles[i]))):
-        p = p + fotos[cycles[i][j]]
-    f.append(p)
-
-# necesito sacar las distancias de cada ciclo con las distancias de q
-distancias = []
-for i in list(range(len(cycles))):
-    l = 0
-    for j in list(range(len(cycles[i])-1)):
-        l = l + (q[cycles[i][j], cycles[i][j+1]] / 1000)
-    distancias.append(l)
-
-data = list(zip(cycles, distancias, f))
-
-headers = ['Ciclos', 'Tiempo', 'Fotos']
-
-table = tb.tabulate(data, headers= headers, tablefmt='fancy_grid')
-
-print(table)
-
-'''#Imprimir grafo de solución
+#Imprimir grafo de solución
 G = nx.DiGraph()
 G.add_nodes_from(Lugares)
 for i,j in A:
-    for d in drones:
-        if x[d,i,j].x > 0.1:
+    for d in Drones:
+        if x[i,j].x > 0.1:
             G.add_edge(i,j)
 
 nx.draw(G, with_labels=True)
 plt.show()
 
-#Imprimir solución
-'''for d in Drones:
-    for i,j in A:
+G = nx.DiGraph()
+
+for i,j in A:
+    for d in Drones:
         if x[i,j].x > 0.1:
-            print('Dron ', d, ' va de ', i, ' a ', j)'''
+            G.add_edge(i,j)
 
+cycles = list(nx.simple_cycles(G))
 
+# Necestio sacar las fotos tomadas en cada cyclo y la distancia recorrida en cada ciclo y dame las fotos en una lista y la idstancias tambien
+fotos = []
+distancias = []
+for cycle in cycles:
+    fotos_cycle = 0
+    distancias_cycle = 0
+    for i in range(len(cycle)-1):
+        distancias_cycle += q[cycle[i],cycle[i+1]]
+        fotos_cycle += Fotos[cycle[i]]
+        if i == len(cycle)-2:
+            fotos_cycle += Fotos[cycle[i+1]]
+    fotos.append(fotos_cycle)
+    distancias.append(distancias_cycle)
 
+headers = ['Secuencia', 'Fotos', 'Distancia']
+data = list(zip(cycles, fotos, distancias))
+print(tb.tabulate(data, headers=headers, tablefmt='grid'))
 
-
-
-
-
-
-
+print(q[(24,20)])
 
